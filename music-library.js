91e55d1ad53831d4e0cbca4ed73dc9088f6ddb31
Alexandra Sia <alexandrasia@gmail.com>
2017-06-02 15:16:22 -0700

    Add prototypes and fix typo

--- music-library.js
+++ music-library.js
@@ -7,26 +7,28 @@ var Library = function(name, creator) {
 var Playlist = function(name) {
   this.name = name;
   this.tracks = [];
-  this.overalRating = function() {
-    var ratings = this.tracks.map(track => track.rating);
-    return (
-      ratings.reduce(function(a, b) {
-        return a + b;
-      }) / ratings.length
-    );
-  };
-  this.totalDuration = function() {
-    var lengths = this.tracks.map(track => track.length);
-    return lengths.reduce(function(a, b) {
-      return a + b;
-    });
-  };
 };
 
 var Track = function(title, rating, length) {
   this.title = title;
   this.rating = rating;
   this.length = length;
+};
+
+Playlist.prototype.overallRating = function() {
+  var ratings = this.tracks.map(track => track.rating);
+  return (
+    ratings.reduce(function(a, b) {
+      return a + b;
+    }) / ratings.length
+  );
+};
+
+Playlist.prototype.totalDuration = function() {
+  var lengths = this.tracks.map(track => track.length);
+  return lengths.reduce(function(a, b) {
+    return a + b;
+  });
 };
 
 var library = new Library("MyLibrary", "Alex");
@@ -39,4 +41,4 @@ playlist1.tracks.push(new Track("Song2", 3, 400));
 playlist1.tracks.push(new Track("Song3", 2, 200));
 
 var duration = playlist1.totalDuration();
-var avgRating = playlist1.overalRating();
+var avgRating = playlist1.overallRating();
